0585e0e550a9b542f0017df290b69702d85520b8
Justinjovert <justinjoags@gmail.com>
2024-06-11 19:35:16 +0800

    added carousel. unfinished

--- ecommerce.js
+++ ecommerce.js
@@ -26,6 +26,12 @@ overlay.addEventListener('click', () => {
     sidebar.style.display = 'none'
     overlay.style.display = 'none'
     displayImageContainer.style.display = 'none'
+
+    // Deletes all active slide
+    const activeSlides = document.querySelector('[data-slides]').querySelectorAll('[data-active]')
+    activeSlides.forEach(activeSlide => {
+        delete activeSlide.dataset.active
+    })
 })
 
 
@@ -33,26 +39,70 @@ overlay.addEventListener('click', () => {
  * Cycles through the images
  */
 
-const mainDisplay = document.getElementById('mainDisplay')
-mainDisplay.addEventListener('click', () => {
-    displayImageContainer.style.display = 'block'
-    overlay.style.display = 'block'
+const mainDisplay = document.querySelector('[data-display]').querySelectorAll('li')
+mainDisplay.forEach(display => {
+    display.addEventListener('click', () => {
+        let productValue = display.dataset.product
+        let selector = `[data-product="${productValue}"]`
+        const newActiveSlide = document.querySelector('[data-slides]').querySelector(selector)
+
+        //Set active
+        newActiveSlide.dataset.active = 'true'
+
+        displayImageContainer.style.display = 'block'
+        overlay.style.display = 'block'
+    })
 })
 
 
+// When thumbnails are interacted
+const productThumbnails = document.getElementById('productThumbnails')
+const proThumbLists = productThumbnails.querySelectorAll('li')
+proThumbLists.forEach(proThumb => {
+    proThumb.addEventListener('click', () => {
+        const activeThumb = productThumbnails.querySelector('.product-thumbnails-active')
+        if (!proThumb.classList.contains('product-thumbnails-active')) {
+            proThumb.classList.add('product-thumbnails-active')
+            activeThumb.classList.remove('product-thumbnails-active')
 
+            //Change main display
+            let productValue = proThumb.dataset.product
+            let selector = `[data-product="${productValue}"]`
+            const activeDisplay = document.querySelector('[data-display]').querySelector('[data-active]')
+            const newActiveDisplay = document.querySelector('[data-display').querySelector(selector)
+
+            /* //Change content when main display is interacted
+            const slides = document.querySelector('[data-carousel').querySelector('[data-slides]')
+            const activeSlide = slides.querySelector('[data-active]')
+            const newActiveSlide = slides.querySelector(selector) */
+
+            // Set new active for slider
+            /* newActiveSlide.dataset.active = 'true'
+            delete activeSlide.dataset.active */
+            // Set new active for main display
+            newActiveDisplay.dataset.active = 'true'
+            delete activeDisplay.dataset.active
+        }
+
+
+    })
+})
+
+// For when the image is cycled while being displayed in the center of the screen
 const buttons = document.querySelectorAll('[data-carousel-button]')
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const offset = button.dataset.carouselButton === 'next' ? 1 : -1
-        const slides = button.closest('[data-carousel').querySelector('[data-slides]')
+        const slides = button.closest('[data-carousel]').querySelector('[data-slides]')
 
         const activeSlide = slides.querySelector('[data-active]')
         let index = [...slides.children].indexOf(activeSlide) + offset
-        if(index < 0) index = slides.children.length - 1
-        if(index >= slides.children.length) index = 0
+        if (index < 0) index = slides.children.length - 1
+        if (index >= slides.children.length) index = 0
 
         slides.children[index].dataset.active = 'true'
         delete activeSlide.dataset.active
     })
 })
+
+// For when the image is cycled in product/main display
